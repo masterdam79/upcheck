514b205605b5095629b55ed5a232a965513f46df
Richard Reijmers <richard@occhio.nl>
2013-06-06 15:47:27 +0200

    added the script

---
+++ upcheck.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+pingCheck=`ping -c 20 $1 | grep $1: | wc -l`
+
+# Check if ping returns result
+if [ "${pingCheck}" -lt "2" ]
+then
+	# Ping does not return result
+	# Check if ping didn't previously returned result stored in upcheck-${1}-.msg
+	pingCheckMsg=`cat upcheck-${1}-.msg`
+	if [ "${pingCheckMsg}" != "Host unreachable" ]
+	then
+		# Then make alert and store alert in upcheck-${1}-.msg
+		echo "Host unreachable" > upcheck-${1}-.msg
+		#echo "Host $1 unreachable"
+		mail -s "[PROBLEM] Host $1 ping returned < 20%" $2 < /dev/null
+	fi
+else
+	# Ping returns result
+	# Check if ping didn't previously returned result stored in upcheck-${1}-.msg
+	pingCheckMsg=`cat upcheck-${1}-.msg`
+	if [ "${pingCheckMsg}" != "Host reachable" ]
+	then
+		# Then make alert and store alert in upcheck-${1}-.msg
+		echo "Host reachable" > upcheck-${1}-.msg
+		#echo "Host $1 reachable"
+		mail -s "[SOLVED] Host $1 ping returned > 20%" $2 < /dev/null
+	fi
+fi
