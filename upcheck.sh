0e660517d7b5b989e28fa20e0034b01a36913a5e
Richard Reijmers <richard@occhio.nl>
2013-07-19 16:27:13 +0200

    some changes

--- upcheck.sh
+++ upcheck.sh
@@ -5,26 +5,26 @@ pingCheck=`ping -c 20 $1 | grep $1: | wc -l`
 # Check if ping returns enough results
 if [ "${pingCheck}" -lt "2" ]
 then
-	# Ping does NOT return enough results
+	# Ping DOES NOT return enough results
 
 	# Check if ping's previous returned result is stored in upcheck-${1}-.msg
 	pingCheckMsg=`cat ~/upcheck-${1}-.msg`
-	if [ "${pingCheckMsg}" != "Host unreachable" ]
+	if [ "${pingCheckMsg}" != "Host unreachable\n" ]
 	then
 		# Then make alert and store alert in upcheck-${1}-.msg
 		echo "Host unreachable" > ~/upcheck-${1}-.msg
 		echo "Host $1 unreachable"
-		mail -s "[PROBLEM] Host $1 ping returned < 20%" $2 < /dev/null
+		mail -s "[PROBLEM] Host $1 ping returned < 10%" $2 < /dev/null
 	fi
 else
-	# Ping returns result
+	# Ping DOES return enough results
 	# Check if ping didn't previously returned result stored in upcheck-${1}-.msg
 	pingCheckMsg=`cat ~/upcheck-${1}-.msg`
-	if [ "${pingCheckMsg}" != "Host reachable" ]
+	if [ "${pingCheckMsg}" != "Host reachable\n" ]
 	then
 		# Then make alert and store alert in upcheck-${1}-.msg
 		echo "Host reachable" > ~/upcheck-${1}-.msg
 		echo "Host $1 reachable"
-		mail -s "[SOLVED] Host $1 ping returned > 20%" $2 < /dev/null
+		mail -s "[SOLVED] Host $1 ping returned > 10%" $2 < /dev/null
 	fi
 fi
